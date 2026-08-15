86d402bb6ffa10734ec8ee55890ee6bf67d07554
alex <alexmoto116@icloud.com>
2026-08-15 16:04:48 +0200

    Новые марки/данные: Mahindra (27), Deutz (790)
    Mahindra: официальный диагностический мануал Mahindra Truck & Bus Division
    (Cosmo LCV MDI BSIII CRDe, через manualslib.com) - взяты только 27 кодов из
    однозначных текстовых разделов "код: описание". В том же документе есть
    сводная таблица на ~107 кодов, но проверка по уже известным 27 показала
    ~11% случаев, где строка таблицы съезжает на соседний код - та же
    ошибка, что раньше похоронила таблицу Renault. Таблицу не заносим.
    
    Deutz: официальный DTC-лист EMR-L1 (serdia.deutz.com), 790 кодов SPN.FMI,
    без коллизий. Текст сильно шаблонный (компонент + тип неисправности) -
    перевод словарём по обеим половинам отдельно, без смешивания языков:
    295 записей полностью на русском, остальные 495 оставлены как есть
    по-английски (compоненты/типы неисправностей вне словаря) - не гадаем.
    
    'mahindra' и 'deutz' добавлены в KNOWN_PCODE_BRANDS (worker/pcode-api.js,
    scripts/build_pcode.py). brandNames для обеих марок уже существовали.

--- scripts/build_pcode.py
+++ scripts/build_pcode.py
@@ -40,7 +40,8 @@ EN_PATH = os.path.join(ROOT, 'assets', 'dtc.en.js')
 
 # Держать в согласии со списком KNOWN_PCODE_BRANDS в worker/pcode-api.js.
 KNOWN_PCODE_BRANDS = ['volvo', 'mercedes', 'scania', 'shacman', 'tata', 'ashokleyland', 'howo', 'faw', 'jac', 'international', 'powerstroke',
-                       'cumminsisb', 'cumminsislisc', 'cumminsism', 'cumminsisx', 'paccarmx13', 'hino', 'renault', 'caterpillar']
+                       'cumminsisb', 'cumminsislisc', 'cumminsism', 'cumminsisx', 'paccarmx13', 'hino', 'renault', 'caterpillar',
+                       'mahindra', 'deutz']
 
 TEMPLATE = u"""(function(){{
   var b64='{b64}';
--- worker/pcode-api.js
+++ worker/pcode-api.js
@@ -16,7 +16,8 @@
 // Держать в согласии с KNOWN_PCODE_BRANDS/top-level ключами
 // pcode-source/pcode.json в scripts/build_pcode.py.
 var KNOWN_PCODE_BRANDS = ['volvo', 'mercedes', 'scania', 'shacman', 'tata', 'ashokleyland', 'howo', 'faw', 'jac', 'international', 'powerstroke',
-                          'cumminsisb', 'cumminsislisc', 'cumminsism', 'cumminsisx', 'paccarmx13', 'hino', 'renault', 'caterpillar'];
+                          'cumminsisb', 'cumminsislisc', 'cumminsism', 'cumminsisx', 'paccarmx13', 'hino', 'renault', 'caterpillar',
+                          'mahindra', 'deutz'];
 var ALLOWED_ORIGINS = ['https://codetruck.ru'];
 
 function norm(code) {
